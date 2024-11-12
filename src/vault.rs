20529f9dff8e0ee954734d084bf9acfe0d13ea3e
Дмитрий Жвакин <dzhvakin>
2024-11-12 21:38:48 +0500

    Created functions for load/save entries to/from vault.

--- src/vault.rs
+++ src/vault.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::{self},
     io::{Read, Seek, SeekFrom, Write},
 };
@@ -9,10 +10,13 @@ const DB_FILE_PATH: &str = "./db.json";
 
 pub struct Vault {
     _file: fs::File,
+    _entries: HashMap<u16, VaultEntry>,
+    _increment: u16,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct VaultEntry {
+    id: u16,
     login: String,
     password: String,
 }
@@ -26,22 +30,42 @@ impl Vault {
             .open(DB_FILE_PATH)
             .unwrap();
 
-        return Vault { _file: file };
-    }
-
-    pub fn entry_create(login: String, password: String) -> VaultEntry {
-        return VaultEntry { login, password };
+        return Vault {
+            _file: file,
+            _entries: HashMap::new(),
+            _increment: 1,
+        };
     }
 }
 
 impl Vault {
-    pub fn read(&mut self) -> String {
+    pub fn load_from_file(&mut self) {
         let mut content = String::new();
+
         self._file.seek(SeekFrom::Start(0)).unwrap();
         self._file.read_to_string(&mut content).unwrap();
 
-        return content;
+        self._entries = serde_json::from_str(&content).unwrap();
     }
 
-    pub fn save(&mut self, entry: &VaultEntry) {}
+    pub fn save_to_file(&mut self) {
+        let serialized = serde_json::to_string_pretty(&self._entries).unwrap();
+        self._file.seek(SeekFrom::Start(0)).unwrap();
+        self._file.write_all(&serialized.as_bytes()).unwrap();
+    }
+
+    pub fn entry_create(&mut self, login: String, password: String) -> VaultEntry {
+        let id = self._increment;
+        self._increment += 1;
+
+        return VaultEntry {
+            id,
+            login,
+            password,
+        };
+    }
+
+    pub fn entry_save(&mut self, entry: VaultEntry) {
+        self._entries.insert(entry.id, entry);
+    }
 }
