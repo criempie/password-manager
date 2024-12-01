98cbe977ce3b9bf153d43d8b8364ac98080045d9
Дмитрий Жвакин <dzhvakin>
2024-12-01 18:49:16 +0500

    Created function for create unlock key from master key.

--- src/vault/mod.rs
+++ src/vault/mod.rs
@@ -1,8 +1,15 @@
 mod database;
 pub mod entries;
 
+use std::fmt::Write;
+
 use database::Database;
 use entries::ClassicEntry;
+
+const UNLOCK_KEY_ITERATIONS: u8 = 4;
+const UNLOCK_KEY_LENGTH: u8 = 16;
+const UNLOCK_KEY_SALT: &str = "TODO_123123123";
+const UNLOCK_KEY_MEM_SIZE: u32 = 65535;
 
 pub struct Vault {
     db: Database<ClassicEntry>,
@@ -37,5 +44,32 @@ impl Vault {
 
     pub fn entries_get(&self) -> &Vec<ClassicEntry> {
         return self.db.entries_get();
+    }
+
+    // Converts the original user-friendly master password into its hash with big length.
+    pub fn derive_unlock_key(password: String) -> String {
+        let argon = argon2::Argon2::new(
+            argon2::Algorithm::Argon2id,
+            argon2::Version::V0x13,
+            argon2::Params::new(UNLOCK_KEY_MEM_SIZE, UNLOCK_KEY_ITERATIONS.into(), 4, None)
+                .unwrap(),
+        );
+
+        let mut buffer = [0u8; UNLOCK_KEY_LENGTH as usize];
+        argon
+            .hash_password_into(password.as_bytes(), UNLOCK_KEY_SALT.as_bytes(), &mut buffer)
+            .unwrap();
+
+        return Vault::bytes_to_hex_string(&buffer);
+    }
+
+    fn bytes_to_hex_string(bytes: &[u8]) -> String {
+        let mut result = String::new();
+
+        for &byte in bytes {
+            write!(&mut result, "{:02x}", byte).unwrap();
+        }
+
+        return result;
     }
 }
