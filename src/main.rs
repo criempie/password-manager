adc555861b1a4705fdcb5729d1a098cf495e2650
Дмитрий Жвакин <d.zhvakin@esilpower.ru>
2025-03-29 15:06:06 +0500

    Cleared.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::{
-    fs::{self},
-    io::{self},
-    time::{self, Instant, SystemTime},
-};
-
-mod password_generator;
-mod ui;
 mod vault;
-
-use ui::cli::CLI;
-use vault::Vault;
+use vault::{PasswordEntry, Vault_reborn};
 
 fn main() {
-    let mut ui = CLI::new(Vault::new());
+    // let mut ui = CLI::new(Vault::new());
 
-    ui.start();
+    // ui.start();
+
+    let mut vault = Vault_reborn::new();
+    let entry = PasswordEntry::new(
+        Vault_reborn::generate_id(),
+        String::from("Hello world"),
+        String::from("12345sdfkj234"),
+    );
+
+    let id = entry.id.clone();
+    vault.add_entry(entry);
+
+    println!("{:?}", vault.get_entry(id));
 }
--- src/vault/mod.rs
+++ src/vault/mod.rs
@@ -1,75 +1,42 @@
-mod database;
-pub mod entries;
+use uuid::Uuid;
 
-use std::fmt::Write;
-
-use database::Database;
-use entries::ClassicEntry;
-
-const UNLOCK_KEY_ITERATIONS: u8 = 4;
-const UNLOCK_KEY_LENGTH: u8 = 16;
-const UNLOCK_KEY_SALT: &str = "TODO_123123123";
-const UNLOCK_KEY_MEM_SIZE: u32 = 65535;
-
-pub struct Vault {
-    db: Database<ClassicEntry>,
+#[derive(Clone, Debug)]
+pub struct PasswordEntry {
+    pub id: String,
+    pub login: String,
+    password: String,
 }
 
-impl Vault {
-    pub fn new() -> Vault {
-        return Vault {
-            db: Database::new(),
-        };
-    }
-
-    pub fn unlock(&mut self) {
-        self.db.open().unwrap();
-        self.db.sync_from_file().unwrap();
-    }
-
-    pub fn lock(&mut self) {
-        self.db.sync_to_file().unwrap();
-        self.db.close();
-    }
-
-    // TODO: Return reference to entry from db after db.entry_get() is implemented.
-    pub fn entry_create(&mut self, login: &str, password: &str) -> ClassicEntry {
-        let entry = ClassicEntry::new(login, password);
-
-        self.db.entry_insert(entry.clone());
-        self.db.sync_to_file().unwrap();
-
-        return entry;
-    }
-
-    pub fn entries_get(&self) -> &Vec<ClassicEntry> {
-        return self.db.entries_get();
-    }
-
-    // Converts the original user-friendly master password into its hash with big length.
-    pub fn derive_unlock_key(password: String) -> String {
-        let argon = argon2::Argon2::new(
-            argon2::Algorithm::Argon2id,
-            argon2::Version::V0x13,
-            argon2::Params::new(UNLOCK_KEY_MEM_SIZE, UNLOCK_KEY_ITERATIONS.into(), 4, None)
-                .unwrap(),
-        );
-
-        let mut buffer = [0u8; UNLOCK_KEY_LENGTH as usize];
-        argon
-            .hash_password_into(password.as_bytes(), UNLOCK_KEY_SALT.as_bytes(), &mut buffer)
-            .unwrap();
-
-        return Vault::bytes_to_hex_string(&buffer);
-    }
-
-    fn bytes_to_hex_string(bytes: &[u8]) -> String {
-        let mut result = String::new();
-
-        for &byte in bytes {
-            write!(&mut result, "{:02x}", byte).unwrap();
+impl PasswordEntry {
+    pub fn new(id: String, login: String, password: String) -> PasswordEntry {
+        Self {
+            id,
+            login,
+            password,
         }
+    }
+}
 
-        return result;
+pub struct Vault_reborn {
+    entries: Vec<PasswordEntry>,
+}
+
+impl Vault_reborn {
+    pub fn new() -> Vault_reborn {
+        Self {
+            entries: Vec::new(),
+        }
+    }
+
+    pub fn generate_id() -> String {
+        return Uuid::new_v4().as_simple().to_string();
+    }
+
+    pub fn add_entry(&mut self, entry: PasswordEntry) {
+        self.entries.push(entry);
+    }
+
+    pub fn get_entry(&self, id: String) -> Option<PasswordEntry> {
+        return self.entries.iter().find(|&entry| entry.id == id).cloned();
     }
 }
