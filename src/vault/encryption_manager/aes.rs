b95c80118ca847410696e3da16c71c2bf19ef024
Дмитрий Жвакин <d.zhvakin@esilpower.ru>
2025-04-13 12:12:07 +0500

    refactor: Added EncryptionKey, EncryptionIV to encrypiton_manager module.

--- src/vault/encryption_manager/aes.rs
+++ src/vault/encryption_manager/aes.rs
@@ -9,15 +9,15 @@ type KeyBytes = [u8; 32];
 type IVBytes = [u8; 16];
 
 pub struct AESEncryptionManager {
-    key: KeyBytes,
+    key: EncryptionKey,
 }
 
 pub struct AESEncryptionParams {
-    iv: IVBytes,
+    iv: EncryptionIV,
 }
 
 impl AESEncryptionManager {
-    pub fn new(key: [u8; 32]) -> Self {
+    pub fn new(key: EncryptionKey) -> Self {
         Self { key }
     }
 }
@@ -26,15 +26,15 @@ impl IEncryptionManager for AESEncryptionManager {
     type Params = AESEncryptionParams;
 
     fn encrypt(&self, data: &[u8], params: &Self::Params) -> Result<Vec<u8>, error::Error> {
-        return encrypt_aes_cbc(data, &self.key, &params.iv);
+        return encrypt_aes_cbc(data, &self.key.bytes(), &params.iv.bytes());
     }
 
     fn decrypt(&self, data: &[u8], params: &Self::Params) -> Result<Vec<u8>, error::Error> {
-        return decrypt_aes_cbc(data, &self.key, &params.iv);
+        return decrypt_aes_cbc(data, &self.key.bytes(), &params.iv.bytes());
     }
 }
 
-pub fn encrypt_aes_cbc(data: &[u8], key: &KeyBytes, iv: &IVBytes) -> Result<Vec<u8>, error::Error> {
+fn encrypt_aes_cbc(data: &[u8], key: &KeyBytes, iv: &IVBytes) -> Result<Vec<u8>, error::Error> {
     type Encryptor = cbc::Encryptor<Aes256>;
 
     // Create a buffer with enough space for padding
@@ -51,7 +51,7 @@ pub fn encrypt_aes_cbc(data: &[u8], key: &KeyBytes, iv: &IVBytes) -> Result<Vec<
     return Ok(encrypted_data.to_vec());
 }
 
-pub fn decrypt_aes_cbc(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, error::Error> {
+fn decrypt_aes_cbc(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, error::Error> {
     type Decryptor = cbc::Decryptor<Aes256>;
 
     let cipher =
@@ -64,4 +64,21 @@ pub fn decrypt_aes_cbc(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, er
         .map_err(|_| error::Error::UnPaddingError)?;
 
     return Ok(buffer);
+}
+
+#[derive(Clone)]
+pub struct EncryptionKey(KeyBytes);
+#[derive(Clone)]
+pub struct EncryptionIV(IVBytes);
+
+impl EncryptionKey {
+    pub fn bytes(&self) -> KeyBytes {
+        return self.0;
+    }
+}
+
+impl EncryptionIV {
+    pub fn bytes(&self) -> IVBytes {
+        return self.0;
+    }
 }
