bf5531661a81cbf4a175327f158a0f9b6b2b25f3
Дмитрий Жвакин <d.zhvakin@esilpower.ru>
2025-04-13 12:22:23 +0500

    feat: Added custom serialization from/to base64 string for EncryptionKey, EncryptionIV.

--- src/vault/encryption_manager/aes.rs
+++ src/vault/encryption_manager/aes.rs
@@ -3,6 +3,9 @@ use aes::{
     Aes256,
 };
 use rand::Rng;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+use crate::vault::base64string::Base64String;
 
 use super::{error, IEncryptionManager};
 
@@ -97,5 +100,47 @@ impl EncryptionIV {
 
     pub fn bytes(&self) -> IVBytes {
         return self.0;
+    }
+}
+
+impl Serialize for EncryptionKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        return serializer.serialize_str(&Base64String::encode(self.0.to_vec()).0);
+    }
+}
+
+impl<'de> Deserialize<'de> for EncryptionKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let base64_data = String::deserialize(deserializer)?;
+        let data = Base64String::from(base64_data).decode().unwrap();
+
+        return Ok(EncryptionKey(data.try_into().unwrap()));
+    }
+}
+
+impl Serialize for EncryptionIV {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        return serializer.serialize_str(&Base64String::encode(self.0.to_vec()).0);
+    }
+}
+
+impl<'de> Deserialize<'de> for EncryptionIV {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let base64_data = String::deserialize(deserializer)?;
+        let data = Base64String::from(base64_data).decode().unwrap();
+
+        return Ok(EncryptionIV(data.try_into().unwrap()));
     }
 }
